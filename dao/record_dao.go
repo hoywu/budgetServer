586b5794097e2cf33b8ee10a20617c3cb37fdcec
hoywu <wu_git@outlook.com>
2024-03-10 18:05:34 +0800

    feat(dao): make get record list return in reverse time order

--- dao/record_dao.go
+++ dao/record_dao.go
@@ -19,7 +19,7 @@ func DeleteRecord(id uint) error {
 
 func GetRecordsByUserID(uid uint, limit int) (*[]model.Record, error) {
 	var records *[]model.Record
-	err := db.DB.Where("user_id = ?", uid).Limit(limit).Find(&records).Error
+	err := db.DB.Where("user_id = ?", uid).Limit(limit).Order("time desc").Find(&records).Error
 	if err != nil {
 		return nil, err
 	}
